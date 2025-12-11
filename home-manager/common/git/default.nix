d5946cb29439b62dcfc33749bcf3866fedebcd7e
Andy Jimenez Reyes <wasosky313@gmail.com>
2025-12-11 14:12:21 -0300

    refactor: restructure git user configuration for clarity

--- home-manager/common/git/default.nix
+++ home-manager/common/git/default.nix
@@ -3,8 +3,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "Andy Jimenez Reyes";
-    userEmail = "wasosky313@gmail.com";
+    settings = {
+        user = {
+          name = "Andy Jimenez Reyes";
+          email = "wasosky313@gmail.com";
+        };
+    };
     signing = {
       key = "B99BD7CB52F8B2BB";
       signByDefault = true;
