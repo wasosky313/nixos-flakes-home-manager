d52d177a4e6449d1f9909b438fc1fdb676abeb3e
Andy Jimenez Reyes <wasosky313@gmail.com>
2025-12-11 14:24:04 -0300

    refactor: add dconf settings to swap ESC and CAPS LOCK keys

--- home-manager/desktop/gnome/default.nix
+++ home-manager/desktop/gnome/default.nix
@@ -4,4 +4,11 @@
   home.packages = with pkgs; [
     pinentry-gnome3
   ];
+
+  dconf.settings = {
+    # swap ESC and CAPS LOCK keys
+    "org/gnome/desktop/input-sources" = {
+      xkb-options = [ "caps:swapescape" ];
+    };
+  };
 }
