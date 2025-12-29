4d74d6781d3059d77a018dd9c78d41f2eab6e49d
Andy Jimenez Reyes <wasosky313@gmail.com>
2025-12-29 14:18:40 -0300

    add vsftp

--- host/configuration.nix
+++ host/configuration.nix
@@ -5,6 +5,7 @@
     [
       ./hardware-configuration.nix
       ./modules/rkvm-client.nix
+      ./modules/vsftpd.nix
     ];
 
   boot.loader = {
---
+++ host/modules/vsftpd.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+{
+  networking.firewall = {
+    allowedTCPPorts = [ 21 ]; # FTP
+    allowedTCPPortRanges = [
+      { from = 50000; to = 50100; } # FTP passive mode
+    ];
+  };
+
+  services.vsftpd = {
+    enable = true;
+    writeEnable = true;
+    localUsers = true;
+    userlist = [ "wasa" ]; # Usuários permitidos
+    userlistEnable = true;
+    anonymousUser = false;
+    anonymousUploadEnable = false;
+    anonymousMkdirEnable = false;
+    chrootlocalUser = true;
+    allowWriteableChroot = true;
+    extraConfig = ''
+      pasv_min_port=50000
+      pasv_max_port=50100
+      pasv_enable=YES
+      pasv_promiscuous=YES
+    '';
+  };
+}
