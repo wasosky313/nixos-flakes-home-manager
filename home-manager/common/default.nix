29853a2e1f4444168c20469f7158eaf1a8cc9d7c
Andy Jimenez Reyes <wasosky313@gmail.com>
2025-12-31 12:33:59 -0300

    refactor: update GPG settings and Git signing key

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -12,4 +12,14 @@
     vlc
     postgresql_17
   ];
+
+  programs.gpg = {
+    enable = true;
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    pinentry.package = pkgs.pinentry-gnome3;
+    enableSshSupport = true;
+  };
 }
