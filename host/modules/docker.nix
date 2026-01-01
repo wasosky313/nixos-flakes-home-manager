dd1c386983a6d7c9940b55bf69ddebfc244b4c73
Andy Jimenez Reyes <wasosky313@gmail.com>
2026-01-01 14:02:59 -0300

    refactor: clean up comments in docker configuration

--- host/modules/docker.nix
+++ host/modules/docker.nix
@@ -1,12 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # Enable Docker
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
   };
 
-  # Add user to docker group
   users.users.wasa.extraGroups = [ "docker" ];
 }
